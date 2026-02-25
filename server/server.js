2db7b6e771c872aa783a6c8c2701e10c0249ba84
ZAEEM-NADEEM-06 <zaeemnadeem2006@gmail.com>
2026-02-25 23:59:14 +0500

    last

--- server/server.js
+++ server/server.js
@@ -27,7 +27,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
   'http://127.0.0.1:3000',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://textile-designer.vercel.app',  // ← Add your frontend URL
 ];
 
 app.use(cors({
